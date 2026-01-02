726d96adf012f42494fa232cc04f4ef5ad26bd48
cfdi40 <hostinger@cfdi40.com>
2026-01-02 09:21:08 -0500

    ok

--- resources/[gamemodes]/colombia-rp/client/phone.lua
+++ resources/[gamemodes]/colombia-rp/client/phone.lua
@@ -92,6 +92,56 @@ function closePhone()
     
     phoneVisible = false
     
+    -- Restaurar controles del juego INMEDIATAMENTE antes de destruir el navegador
+    showCursor(false)
+    guiSetInputEnabled(false)
+    guiSetInputMode("allow_binds")
+    
+    -- Forzar restauración de controles inmediatamente
+    toggleControl("fire", true)
+    toggleControl("next_weapon", true)
+    toggleControl("previous_weapon", true)
+    toggleControl("forwards", true)
+    toggleControl("backwards", true)
+    toggleControl("left", true)
+    toggleControl("right", true)
+    toggleControl("jump", true)
+    toggleControl("sprint", true)
+    toggleControl("crouch", true)
+    toggleControl("walk", true)
+    toggleControl("enter_exit", true)
+    toggleControl("vehicle_fire", true)
+    toggleControl("vehicle_secondary_fire", true)
+    toggleControl("steer_forward", true)
+    toggleControl("steer_back", true)
+    toggleControl("accelerate", true)
+    toggleControl("brake_reverse", true)
+    toggleControl("handbrake", true)
+    toggleControl("horn", true)
+    toggleControl("sub_mission", true)
+    toggleControl("radio_next", true)
+    toggleControl("radio_previous", true)
+    toggleControl("radio_user_track_skip", true)
+    toggleControl("vehicle_look_left", true)
+    toggleControl("vehicle_look_right", true)
+    toggleControl("vehicle_look_behind", true)
+    toggleControl("vehicle_mouse_look", true)
+    toggleControl("special_control_left", true)
+    toggleControl("special_control_right", true)
+    toggleControl("special_control_up", true)
+    toggleControl("special_control_down", true)
+    toggleControl("aim_weapon", true)
+    toggleControl("conversation_yes", true)
+    toggleControl("conversation_no", true)
+    toggleControl("group_control_forwards", true)
+    toggleControl("group_control_back", true)
+    toggleControl("enter_passenger", true)
+    toggleControl("cycle_weapon_left", true)
+    toggleControl("cycle_weapon_right", true)
+    toggleControl("change_camera", true)
+    toggleControl("vehicle_next_radio", true)
+    toggleControl("vehicle_prev_radio", true)
+    
     if phoneBrowser and isElement(phoneBrowser) then
         removeEventHandler("onClientBrowserCreated", phoneBrowser, loadPhoneBrowser)
         removeEventHandler("onClientBrowserDocumentReady", phoneBrowser, whenPhoneBrowserReady)
@@ -102,17 +152,10 @@ function closePhone()
     
     renderTime(false)
     
-    -- Restaurar controles del juego
-    showCursor(false)
-    guiSetInputEnabled(false)
-    guiSetInputMode("allow_binds")
-    
-    -- Asegurar que el jugador pueda moverse y usar controles
+    -- Asegurar que los controles se mantengan habilitados después de un pequeño delay
     setTimer(function()
-        -- Forzar restauración de controles
+        -- Forzar restauración de controles nuevamente por si acaso
         toggleControl("fire", true)
-        toggleControl("next_weapon", true)
-        toggleControl("previous_weapon", true)
         toggleControl("forwards", true)
         toggleControl("backwards", true)
         toggleControl("left", true)
@@ -121,39 +164,8 @@ function closePhone()
         toggleControl("sprint", true)
         toggleControl("crouch", true)
         toggleControl("walk", true)
-        toggleControl("enter_exit", true)
-        toggleControl("vehicle_fire", true)
-        toggleControl("vehicle_secondary_fire", true)
-        toggleControl("steer_forward", true)
-        toggleControl("steer_back", true)
-        toggleControl("accelerate", true)
-        toggleControl("brake_reverse", true)
-        toggleControl("handbrake", true)
-        toggleControl("horn", true)
-        toggleControl("sub_mission", true)
-        toggleControl("radio_next", true)
-        toggleControl("radio_previous", true)
-        toggleControl("radio_user_track_skip", true)
-        toggleControl("vehicle_look_left", true)
-        toggleControl("vehicle_look_right", true)
-        toggleControl("vehicle_look_behind", true)
-        toggleControl("vehicle_mouse_look", true)
-        toggleControl("special_control_left", true)
-        toggleControl("special_control_right", true)
-        toggleControl("special_control_up", true)
-        toggleControl("special_control_down", true)
-        toggleControl("aim_weapon", true)
-        toggleControl("conversation_yes", true)
-        toggleControl("conversation_no", true)
-        toggleControl("group_control_forwards", true)
-        toggleControl("group_control_back", true)
-        toggleControl("enter_passenger", true)
-        toggleControl("cycle_weapon_left", true)
-        toggleControl("cycle_weapon_right", true)
-        toggleControl("change_camera", true)
-        toggleControl("vehicle_next_radio", true)
-        toggleControl("vehicle_prev_radio", true)
-    end, 100, 1)
+        guiSetInputMode("allow_binds")
+    end, 50, 1)
 end
 
 -- Evento del servidor para abrir el teléfono
@@ -217,14 +229,17 @@ end)
 -- Evento para cerrar el teléfono desde el navegador (botón home cuando ya está en home)
 addEvent("closePhoneFromBrowser", true)
 addEventHandler("closePhoneFromBrowser", localPlayer, function()
-    -- Guardar contactos antes de cerrar
+    -- Guardar contactos antes de cerrar (si el navegador aún existe)
     if browserContent and isElement(browserContent) then
         executeBrowserJavascript(browserContent, "saveContactsToMTA();")
-    end
-    -- Pequeño delay para asegurar que se guarden los contactos
-    setTimer(function()
+        -- Pequeño delay para asegurar que se guarden los contactos, luego cerrar
+        setTimer(function()
+            closePhone()
+        end, 50, 1)
+    else
+        -- Si el navegador ya no existe, cerrar inmediatamente
         closePhone()
-    end, 100, 1)
+    end
 end)
 
 -- Cerrar teléfono con tecla ESC
