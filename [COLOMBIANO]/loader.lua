8df1b97c23f748bc5a464258257bb7ab4bfbe7c9
jaimezvictor82-spec <jaimezvictor82@gmail.com>
2026-01-02 14:17:17 -0500

    Skin 0 Mta

---
+++ [COLOMBIANO]/loader.lua
@@ -0,0 +1,37 @@
+
+local files = {
+	{ "[COLOMBIANO].txd.pcrypt", 30 },
+	{ "[COLOMBIANO].dff.pcrypt", 30 },
+}
+
+
+local cor = coroutine.create(function()
+    for i = 1, #files do
+        local file  = files[i][1]
+        local model = files[i][2]
+
+        pDecrypt(file, function(data)
+            loadModel(file, data, model)
+        end)
+        coroutine.yield()
+    end
+
+    files = nil
+    collectgarbage()
+end)
+
+function loadModel(file, data, model)
+    local ext = file:match("^.+(%..+)%..+$")
+
+    if ext == ".dff" then
+        engineReplaceModel(engineLoadDFF(data), model)
+    elseif ext == ".txd" then
+        engineImportTXD(engineLoadTXD(data), model)
+    elseif ext == ".col" then
+        engineReplaceCOL(engineLoadCOL(data), model)
+    end
+
+    coroutine.resume(cor)
+end
+
+coroutine.resume(cor)
