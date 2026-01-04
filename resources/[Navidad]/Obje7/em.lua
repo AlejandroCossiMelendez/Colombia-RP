6d2cf894102080d1a51c3bbc4c80bdc46c693364
Proyecto Colombia <miguemosquera95@gmail.com>
2026-01-04 16:49:01 -0500

    navidad

--- resources/[Navidad]/Obje7/em.lua
+++
@@ -1,9 +0,0 @@
-function replaceModel()
-  txd = engineLoadTXD("em.txd", 1727 )
-  engineImportTXD(txd, 1727)
-  dff = engineLoadDFF("em.dff", 1727 )
-  engineReplaceModel(dff, 1727)
-  col= engineLoadCOL ( "em.col" )
-  engineReplaceCOL ( col, 1727 )
-end
-addEventHandler ( "onClientResourceStart", getResourceRootElement(getThisResource()), replaceModel)
