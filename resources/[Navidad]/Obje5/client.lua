192f8c9f2a79fa7edb63f69f427c3974905f34c0
Proyecto Colombia <miguemosquera95@gmail.com>
2026-01-04 16:33:16 -0500

    [Navidad]

---
+++ resources/[Navidad]/Obje5/client.lua
@@ -0,0 +1,17 @@
+﻿col = engineLoadCOL ( "object.col" )
+engineReplaceCOL ( col, 1869 )
+txd = engineLoadTXD ( "object.txd" )
+engineImportTXD ( txd, 1869 )
+dff = engineLoadDFF ( "object.dff" )
+engineReplaceModel ( dff, 1869)
+engineSetModelLODDistance ( 1869, 9999)
+
+
+
+-- Sitemiz : https://sparrow-mta.blogspot.com/
+
+-- Facebook : https://facebook.com/sparrowgta/
+-- İnstagram : https://instagram.com/sparrowmta/
+-- YouTube : https://www.youtube.com/@TurkishSparroW/
+
+-- Discord : https://discord.gg/DzgEcvy
---
+++ resources/[Navidad]/Obje6/client.lua
@@ -0,0 +1,28 @@
+﻿col = engineLoadCOL ( "object.col" )
+engineReplaceCOL ( col, 1866 )
+txd = engineLoadTXD ( "object.txd" )
+engineImportTXD ( txd, 1866 )
+dff = engineLoadDFF ( "object.dff" )
+engineReplaceModel ( dff, 1866)
+engineSetModelLODDistance ( 1866, 9999)
+
+--- 2
+
+cola = engineLoadCOL ( "object1.col" )
+engineReplaceCOL ( cola, 1867 )
+txda = engineLoadTXD ( "object.txd" )
+engineImportTXD ( txd1, 1867 )
+dffa = engineLoadDFF ( "object1.dff" )
+engineReplaceModel ( dffa, 1867)
+engineSetModelLODDistance ( 1867, 9999)
+
+-----
+
+colb = engineLoadCOL ( "object2.col" )
+engineReplaceCOL ( colb, 1868 )
+txdb = engineLoadTXD ( "object.txd" )
+engineImportTXD ( txdb, 1868 )
+dffb = engineLoadDFF ( "object2.dff" )
+engineReplaceModel ( dffb, 1868)
+engineSetModelLODDistance ( 1868, 9999)
+
